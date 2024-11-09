4aa44ea5879ab6bc95ed63bc068ec7a26d72daf2
Willian Binda <willianbinda122@gmail.com>
2024-11-09 20:58:01 -0300

    feat: ajustando gr

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,3 +1,41 @@
+interface GR {
+  [key: string]: string[];
+}
+
+const terminaComEdison = (gr: GR[], naoTerminalAtual: string) => {
+  const regraEncontrada = gr.find((obj) =>
+    obj.hasOwnProperty(naoTerminalAtual)
+  );
+
+  // console.log("Estado atual: ", naoTerminalAtual);
+  // console.log("Verificar se existe & na regra atual: ", regraEncontrada);
+
+  if (regraEncontrada) {
+    const options = regraEncontrada[naoTerminalAtual];
+
+    const optionEncontrada = options.find((opt) => opt === "&");
+
+    if (optionEncontrada) {
+      // console.log("Edison encontrado:", optionEncontrada);
+      return true;
+    } else {
+      throw new Error("Entrada invalida!");
+      // console.log("entrada não encontrada");
+      // throw new Error(
+      //   "Gramática incorreta terminal não foi declarado corretamente nas regras."
+      // );
+    }
+  } else {
+    // console.log(
+    //   "nao terminal não foi encontrado na utlima verificação de terminação"
+    // );
+
+    throw new Error(
+      "nao terminal não foi encontrado na utlima verificação de terminação"
+    );
+  }
+};
+
 function procuraOpcao(options: string[], searchTerm: string) {
   // Primeiro tenta encontrar o caractere exato
   let found = options.find((element) => element === searchTerm);
@@ -12,66 +50,80 @@ function procuraOpcao(options: string[], searchTerm: string) {
 }
 
 export const validarGramatica = (gr: string, entrada: string) => {
-  // separa a string das regras em array  ['S->aA|bB', 'A->aA|&', 'B->bB|&']
+  //   console.log("gramatica string:", gr);
   const gramatica = gr.split("\n");
-  //   console.log(gramatica);
 
-  // trasnforma o array em objeto [{S: ['aA','aB']}, {A: ['aA','&']}, {B: ['bB','&']}]
+  // console.log("gramatica:", gramatica);
+
   const novaGramatica = gramatica.map((item, index) => {
     const naoTerminal = item[0];
     return {
       [naoTerminal]: gramatica[index].split(naoTerminal + "->")[1].split("|"),
     };
   });
-  //   console.log(novaGramatica);
+
+  //   console.log("nova gramatica: ", novaGramatica);
+
   // eslint-disable-next-line
   let naoTerminalAtual = "S";
 
-  //percorrer e validar entrada
   entrada.split("").forEach((char, index) => {
-    // ultimo caractere da entrada
     if (entrada.length - 1 === index) {
-      //procura no array de objeto se possui o não terminal como chave
+      //   console.log("******************");
+      //   console.log("******************");
+      //   console.log(
+      //     "ultima derivação deve terminal no & ou na regra que possui a ultima entrada"
+      //   );
+      //   console.log("ultimo caractere: ", char);
+      //   console.log("Não terminal atual: ", naoTerminalAtual);
+      //   throw new Error("ultima derivação deve terminal no &");
+
       const regraEncontrada = novaGramatica.find((obj) =>
         obj.hasOwnProperty(naoTerminalAtual)
       );
 
       if (regraEncontrada) {
-        // pega o array do não terminal
+        // console.log("regra: ", regraEncontrada);
         const options = regraEncontrada[naoTerminalAtual];
+        // console.log("options: ", options);
 
-        // procura se tem edison ou nao retonar 'aA' = caso edison ou 'a' = não edison
         const optionEncontrada = procuraOpcao(options, char);
+        // const optionEncontrada = options.find(
+        //   (opt) => opt === char || regex.test(opt)
+        // );
 
         if (optionEncontrada) {
-          // sem termina com edison
-          if (optionEncontrada.length === 2) {
-            // separa a opção 'aA' em ['a','A'] e retorna o index 1 = 'A'
+          //   console.log("entrada encontrada ultima rep: ", optionEncontrada);
+          //   console.log(optionEncontrada.length);
+          if (optionEncontrada.length !== 1) {
+            //termina com edison
+            // console.log("regras com edison");
             const naoTerminalRegra = optionEncontrada.split("")[1];
-
-            //procura na gr o não terminal
-            const regraEncontrada = novaGramatica.find((obj) =>
-              obj.hasOwnProperty(naoTerminalAtual)
-            );
-
-            if (regraEncontrada) {
-              const options = regraEncontrada[naoTerminalRegra];
-              // procura se tem o edison nas opções
-              const optionEncontrada = options.find((opt) => opt === "&");
-
-              if (!optionEncontrada) {
-                throw new Error("Entrada invalida!");
-              }
-            } else {
-              throw new Error(
-                "nao terminal não foi encontrado na utlima verificação de terminação"
-              );
-            }
+            // console.log("opções da regra: ", naoTerminalRegra);
+            // const uppercaseChars = naoTerminalRegra.find(
+            //   (charRegra) => charRegra === charRegra.toUpperCase()
+            // );
+            // console.log("Não terminal da regra da linha: ", naoTerminalRegra);
+            // naoTerminalAtual = naoTerminalRegra;
+            terminaComEdison(novaGramatica, naoTerminalRegra);
+            // console.log("edison encontrado");
           }
+
+          // if (uppercaseChars) {
+          //   // naoTerminalAtual = uppercaseChars
+          // } else {
+          //   console.log("Não terminal não foi encontrado na regra");
+          //   throw new Error(
+          //     "Gramática incorreta não terminal não foi encontrado na derivação."
+          //   );
+          // }
         } else {
+          //   console.log("entrada não encontrada");
           throw new Error("entrada não encontrada");
         }
       } else {
+        // console.log("nao terminal não foi encontrado");
+
         throw new Error("nao terminal não foi encontrado");
       }
 
@@ -82,22 +134,42 @@ export const validarGramatica = (gr: string, entrada: string) => {
       );
 
       if (regraEncontrada) {
+        // console.log("regra: ", regraEncontrada);
         const options = regraEncontrada[naoTerminalAtual];
+        // console.log("options: ", options);
 
         const searchTerm = char;
         const regex = new RegExp(`^${searchTerm}`);
 
         const optionEncontrada = options.find((opt) => regex.test(opt));
         if (optionEncontrada) {
+          //   console.log("entrada encontrada: ", optionEncontrada);
           const naoTerminalRegra = optionEncontrada.split("")[1];
-
+          // console.log("opções da regra: ", naoTerminalRegra);
+          // const uppercaseChars = naoTerminalRegra.find(
+          //   (charRegra) => charRegra === charRegra.toUpperCase()
+          // );
+          //   console.log("Não terminal da regra da linha: ", naoTerminalRegra);
           naoTerminalAtual = naoTerminalRegra;
+          // if (uppercaseChars) {
+          //   // naoTerminalAtual = uppercaseChars
+          // } else {
+          //   console.log("Não terminal não foi encontrado na regra");
+          //   throw new Error(
+          //     "Gramática incorreta não terminal não foi encontrado na derivação."
+          //   );
+          // }
         } else {
+          //   console.log("entrada não encontrada");
           throw new Error("entrada não encontrada");
         }
       } else {
+        // console.log("nao terminal não foi encontrado");
+
         throw new Error("nao terminal não foi encontrado");
       }
     }
   });
+
+  //verificar se a ultima regra possui &
 };
